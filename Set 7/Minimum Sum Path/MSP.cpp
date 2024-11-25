6cb42c7f733dfd8f08e1c742b7990f1919df6040
Stephen Paul <Stephenpaul4040@gmail.com>
2024-11-25 12:58:10 +0530

    Fixed code

--- Set 7/Minimum Sum Path/MSP.cpp
+++ Set 7/Minimum Sum Path/MSP.cpp
@@ -3,46 +3,48 @@
 #include <climits>
 using namespace std;
 
-int helper(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
-    if (i == 0 && j == 0) {
-        return matrix[0][0];
-    }
-    if (i < 0 || j < 0) {
-        return INT_MAX;
-    }
-    if (dp[i][j] != -1) {
-        return dp[i][j];
-    }
-    int up = matrix[i][j] + helper(i - 1, j, matrix, dp);
-    int left = matrix[i][j] + helper(i, j - 1, matrix, dp);
-    dp[i][j] = min(up, left);
-    return dp[i][j];
+int min(int x, int y, int z) {
+    if (x < y)
+        return (x < z) ? x : z;
+    else
+        return (y < z) ? y : z;
 }
 
-int minSumPath(int n, int m, vector<vector<int>>& matrix) {
-    vector<vector<int>> dp(n, vector<int>(m, -1));
-    return helper(n - 1, m - 1, matrix, dp);
+int minCost(vector<vector<int>>& cost, int m, int n) {
+    if (m < 0 || n < 0)
+        return INT_MAX;
+    else if (m == 0 && n == 0)
+        return cost[m][n];
+    else
+        return cost[m][n] + min(minCost(cost, m - 1, n - 1),
+                                minCost(cost, m - 1, n),
+                                minCost(cost, m, n - 1));
 }
 
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cout << "Enter the number of rows in the matrix: ";
-        cin >> n;
-        cout << "Enter the number of columns in the matrix: ";
-        cin >> m;
+    int T;
+    cout << "Enter the number of test cases: ";
+    cin >> T;
 
-        vector<vector<int>> matrix(n, vector<int>(m));
-        cout << "Enter the matrix row by row:" << endl;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                cin >> matrix[i][j];
+    while (T--) {
+        int R, C;
+        cout << "Enter the number of rows and columns: ";
+        cin >> R >> C;
+
+        vector<vector<int>> cost(R, vector<int>(C));
+        cout << "Enter the cost matrix row by row:\n";
+        for (int i = 0; i < R; ++i) {
+            for (int j = 0; j < C; ++j) {
+                cin >> cost[i][j];
             }
         }
 
-        cout << "The minimum sum path is: " << minSumPath(n, m, matrix) << endl;
+        int m, n;
+        cout << "Enter the target cell indices (m, n): ";
+        cin >> m >> n;
+
+        cout << "Minimum cost to reach (" << m << ", " << n << "): " << minCost(cost, m, n) << endl;
     }
+
     return 0;
 }
